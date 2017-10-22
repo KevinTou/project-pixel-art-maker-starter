b7035a24876ad561d85ab2619b9bc1f17e68c8a3
Kevin Tou <toukevin@gmail.com>
2017-10-21 18:18:31 -0700

    Added makeGrid function

--- designs.js
+++ designs.js
@@ -4,7 +4,14 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+    let row = $('#input_height').val();
+    let column = $('#input_width').val();
+    for (let i = 0; i < row; ++i) {
+        var rows = $('<tr class="cells">');
+        $('#pixel_canvas').append(rows);
+        for (let j = 0; j < column; ++j) {
+            var cell = $('<td class="cells">');
+            rows.append(cell);
+        }
+    }
 }
